af108b8edb40f638400e7d7729f339fd8473fbe6
MindSpark-fyp <uswaarif2021cs77b@gmail.com>
2025-04-15 14:42:38 +0500

    https

--- Frontend/src/components/Config.js
+++ Frontend/src/components/Config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://3.109.206.115:5000";
+const API_BASE_URL = "https://3.109.206.115:5000";
 //const API_BASE_URL = "http://localhost:5000";
 
 export default API_BASE_URL;
